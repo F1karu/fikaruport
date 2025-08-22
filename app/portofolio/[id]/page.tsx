d1c6e4fe7ded930e9f2b7a0e1b5e05cc8d4d9950
FIKARU <dzulfikarky@gmail.com>
2025-08-22 16:44:29 +0700

    param

--- app/portofolio/[id]/page.tsx
+++ app/portofolio/[id]/page.tsx
@@ -4,7 +4,7 @@ import { useState, useEffect, Suspense } from "react";
 import { useSearchParams } from "next/navigation";
 import Link from "next/link";
 import Image from "next/image";
-import { arrayPorto } from "../../data/portofolio";
+import { arrayPorto } from "../../../data/portofolio";
 
 export default function PortofolioPage() {
   const [fade, setFade] = useState(false);
